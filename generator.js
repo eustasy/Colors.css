77a83b67c08eb58d4407abe3fb45a14630ea22e7
Lewis Goddard <lewisgoddard@eustasy.org>
2017-07-12 17:30:32 +0100

    Add generator, colors.css, new palette

---
+++ generator.js
@@ -0,0 +1,26 @@
+$(function() {
+	$.getJSON('colors.json', function( data ) {
+		var css = '/*! Colors.css 1.7 | MIT License | https://github.com/eustasy/colors.css */\n'
+		//console.log(data)
+		$.each( data, function( group, list ) {
+			//console.log(group + ': ' + list)
+			css += '/* $$ ' + group + ' */\n'
+			css += '/* $ Backgrounds */\n'
+			$.each( list, function( key, val ) {
+				//console.log(key + ': ' + val)
+				css += '.background-' + key.replace(/\s+/g, '-').toLowerCase() + ' { background: ' + val.toLowerCase() + '; }\n'
+			})
+			css += '/* $ Colors */\n'
+			$.each( list, function( key, val ) {
+				//console.log(key + ': ' + val)
+				css += '.color-' + key.replace(/\s+/g, '-').toLowerCase() + ' { color: ' + val.toLowerCase() + '; }\n'
+			})
+			css += '/* $ Fills */\n'
+			$.each( list, function( key, val ) {
+				//console.log(key + ': ' + val)
+				css += '.fill-' + key.replace(/\s+/g, '-').toLowerCase() + ' { fill: ' + val.toLowerCase() + '; }\n'
+			})
+		})
+		$('#js-target-css').val(css)
+	})
+})
