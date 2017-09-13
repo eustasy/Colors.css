2700e5c90f0c6fe681fc78946babff6d17cebeb6
Lewis Goddard <lewisgoddard@eustasy.org>
2017-09-13 01:06:34 +0100

    Update to 1.9.0, fix #8

--- generator.js
+++ generator.js
@@ -1,7 +1,7 @@
 /*global $*/
 $(function() {
 	$.getJSON('colors.json', function( data ) {
-		var css = '/*! Colors.css 1.8 | MIT License | https://github.com/eustasy/colors.css */\n'
+		var css = '/*! Colors.css 1.9.0 | MIT License | https://github.com/eustasy/colors.css */\n'
 		//console.log(data)
 		$.each( data, function( group, list ) {
 			//console.log(group + ': ' + list)
@@ -10,21 +10,21 @@ $(function() {
 			if ( group == 'Baseline' ) {
 				group = ''
 			} else {
-				group = group.replace(/\s+/g, '-').toLowerCase()
+				group = group.replace(/\s+/g, '').toLowerCase() + '-'
 			}
 			$.each( list, function( key, val ) {
 				//console.log(key + ': ' + val)
-				css += '.background-' + key.replace(/\s+/g, '-').toLowerCase() + ' { background: ' + val.toLowerCase() + '; }\n'
+				css += '.background-' + group + key.replace(/\s+/g, '-').toLowerCase() + ' { background: ' + val.toLowerCase() + '; }\n'
 			})
 			css += '/* $ Colors */\n'
 			$.each( list, function( key, val ) {
 				//console.log(key + ': ' + val)
-				css += '.color-' + key.replace(/\s+/g, '-').toLowerCase() + ' { color: ' + val.toLowerCase() + '; }\n'
+				css += '.color-' + group + key.replace(/\s+/g, '-').toLowerCase() + ' { color: ' + val.toLowerCase() + '; }\n'
 			})
 			css += '/* $ Fills */\n'
 			$.each( list, function( key, val ) {
 				//console.log(key + ': ' + val)
-				css += '.fill-' + key.replace(/\s+/g, '-').toLowerCase() + ' { fill: ' + val.toLowerCase() + '; }\n'
+				css += '.fill-' + group + key.replace(/\s+/g, '-').toLowerCase() + ' { fill: ' + val.toLowerCase() + '; }\n'
 			})
 		})
 		$('#js-target-css').val(css)
