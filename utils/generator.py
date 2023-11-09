8c272f7c9d8d4063e87e24acdf3b1f48b19e70b8
Lewis Goddard <lewisgoddard@eustasy.org>
2023-11-09 20:36:12 +0000

    Update generator.py

--- utils/generator.py
+++ utils/generator.py
@@ -34,17 +34,19 @@ for title, slug in index.items():
     #   Step 2. b) Create CSS variables for each color
     css += ':root {\n'
     for color, hex in colors.items():
+        color = re.sub('\s+', '-', color).lower()
         # Should look like --elementary-orange: #f37329;
-        css += '\t--' + slug + '-' + re.sub('\s+', '-', color).lower() + ': ' + hex.lower() + ';\n'
+        css += '\t--' + slug + '-' + color + ': ' + hex.lower() + ';\n'
     css += '}\n'
 
     #   Step 2. c) Create CSS types for each color
     for type in types:
         for color, hex in colors.items():
+            color = re.sub('\s+', '-', color).lower()
             # Should look like .color-elementary-orange
-            css += '.' + type + '-' + slug + '-' + re.sub('\s+', '-', color).lower()
+            css += '.' + type + '-' + slug + '-' + color
             # Should look like { color: #f37329; }
-            css += ' { ' + type + ': var(--' + slug + '); }\n'
+            css += ' { ' + type + ': var(--' + slug + '-' + color); }\n'
 
     #   Step 2. d) Put data into palette.css files
     css_all += css
