b5d4cf3a22e964f19c41b14278577356ae221f50
Lewis Goddard <lewis.goddard@eustasy.org>
2026-06-01 10:54:19 +0100

    Format with ruff

--- utils/generator.py
+++ utils/generator.py
@@ -5,58 +5,60 @@ from collections import OrderedDict
 
 #   Step 0. Initialize veriables
 path = os.path.dirname(os.path.abspath(__file__))
-types = ['background', 'color', 'fill', 'border-color', 'stroke', 'outline-color', 'text-decoration-color', 'caret-color', 'accent-color']
-version = '2.1.0'
-css_all = ''
+types = ["background", "color", "fill", "border-color", "stroke", "outline-color", "text-decoration-color", "caret-color", "accent-color"]
+version = "2.1.0"
+css_all = ""
+
 
 def save_css(path, slug, suffix, css):
-    with open(path + '/../' + slug + suffix, 'w+') as outfile:
+    with open(path + "/../" + slug + suffix, "w+") as outfile:
         outfile.write(css)
 
+
 #   Step 1. Load _meta.json
-with open(path + '/../data/_meta.json') as f:
+with open(path + "/../data/_meta.json") as f:
     index = json.load(f, object_pairs_hook=OrderedDict)
 
 #   Step 2. Create individual palette files
 for slug, info in index.items():
-    css = ''
-    title = info['name']
+    css = ""
+    title = info["name"]
 
     #   Step 2. a) Load palette data
-    with open(path + '/../data/' + slug + '.json') as f:
+    with open(path + "/../data/" + slug + ".json") as f:
         colors = json.load(f, object_pairs_hook=OrderedDict)
 
     #   Step 2. b) Create CSS variables for each color
-    css += ':root {\n'
+    css += ":root {\n"
     for color, hex in colors.items():
-        color = re.sub(r'\s+', '-', color).lower()
+        color = re.sub(r"\s+", "-", color).lower()
         # Should look like --elementary-orange: #f37329;
-        css += '\t--' + slug + '-' + color + ': ' + hex.lower() + ';\n'
-    css += '}\n'
+        css += "\t--" + slug + "-" + color + ": " + hex.lower() + ";\n"
+    css += "}\n"
 
     #   Step 2. c) Create CSS types for each color
     for type in types:
         for color in colors:
-            color = re.sub(r'\s+', '-', color).lower()
+            color = re.sub(r"\s+", "-", color).lower()
             # Should look like .color-elementary-orange
-            css += '.' + type + '-' + slug + '-' + color
+            css += "." + type + "-" + slug + "-" + color
             # Should look like { color: #f37329; }
-            css += ' { ' + type + ': var(--' + slug + '-' + color + '); }\n'
+            css += " { " + type + ": var(--" + slug + "-" + color + "); }\n"
 
     #   Step 2. d) Put data into palette.css files
     css_all += css
-    css_title = '/*! Colors.css ' + version + ' | ' + title + ' Palette | MIT License | https://github.com/eustasy/colors.css */\n'
-    css_min = css_title + re.sub(r'[\s\n]+', '', css).lower()
+    css_title = "/*! Colors.css " + version + " | " + title + " Palette | MIT License | https://github.com/eustasy/colors.css */\n"
+    css_min = css_title + re.sub(r"[\s\n]+", "", css).lower()
     css = css_title + css
 
-    save_css(path, slug, '.css', css)
-    save_css(path, slug, '.min.css', css_min)
+    save_css(path, slug, ".css", css)
+    save_css(path, slug, ".min.css", css_min)
 
 #   Step 3. Combine and minify into main files
-slug = 'colors'
-css_all_title = '/*! Colors.css ' + version + ' | All Palettes | MIT License | https://github.com/eustasy/colors.css */\n'
-css_min = css_all_title + re.sub(r'[\s\n]+', '', css_all).lower()
+slug = "colors"
+css_all_title = "/*! Colors.css " + version + " | All Palettes | MIT License | https://github.com/eustasy/colors.css */\n"
+css_min = css_all_title + re.sub(r"[\s\n]+", "", css_all).lower()
 css = css_all_title + css_all
 
-save_css(path, slug, '.css', css)
-save_css(path, slug, '.min.css', css_min)
+save_css(path, slug, ".css", css)
+save_css(path, slug, ".min.css", css_min)
