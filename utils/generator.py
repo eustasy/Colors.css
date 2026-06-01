97bc92c43d2fb0d8d076ee6bcfb03aacfc41fcb1
Lewis Goddard <lewis.goddard@eustasy.org>
2026-06-01 12:14:25 +0100

    2.1.1

--- utils/generator.py
+++ utils/generator.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 #   Step 0. Initialize veriables
 path = os.path.dirname(os.path.abspath(__file__))
 types = ["background", "color", "fill", "border-color", "stroke", "outline-color", "text-decoration-color", "caret-color", "accent-color"]
-version = "2.1.0"
+version = "2.1.1"
 vars_all = ""
 rules_all = ""
 
